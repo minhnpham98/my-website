554e268d94ad5b7733522ca29a6a720f338b4bc8
Minh Pham <minhnpham98@googlemail.com>
2021-10-24 01:21:53 +0200

    Routing to component "home"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'about-me', component: AboutMeComponent },
 ];
 
