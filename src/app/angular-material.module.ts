475e4e0460f51614d11fa4b1cca6cbd4e0c96e3e
Minh Pham <minhnpham98@googlemail.com>
2021-10-24 21:05:56 +0200

    Added first career-steps

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatStepperModule } from '@angular/material/stepper';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 const AngularMaterialComponents = [
     MatButtonModule,
     MatIconModule,
     MatMenuModule,
+    MatStepperModule,
     MatToolbarModule,
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { CareerComponent } from './about-me/career/career.component';
+import { EducationComponent } from './about-me/education/education.component';
+import { SkillsComponent } from './about-me/skills/skills.component';
+import { VolunteeringComponent } from './about-me/volunteering/volunteering.component';
 import { AngularMaterialModule } from './angular-material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { CareerComponent } from './about-me/career/career.component';
-import { EducationComponent } from './about-me/education/education.component';
-import { VolunteeringComponent } from './about-me/volunteering/volunteering.component';
-import { SkillsComponent } from './about-me/skills/skills.component';
-import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 
 @NgModule({
@@ -34,7 +35,12 @@ import { ContactComponent } from './contact/contact.component';
     AngularMaterialModule,
     FlexLayoutModule,
   ],
-  providers: [],
+  providers: [
+    {
+      provide: STEPPER_GLOBAL_OPTIONS,
+      useValue: { displayDefaultIndicatorType: false }
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
