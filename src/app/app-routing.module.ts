61dfbf2ace1ee07832e586cda7e7beb89bf3a7c6
Minh Pham <minhnpham98@googlemail.com>
2021-10-24 17:16:49 +0200

    Clean-up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutMeComponent } from './about-me/about-me.component';
+import { CareerComponent } from './about-me/career/career.component';
+import { EducationComponent } from './about-me/education/education.component';
+import { SkillsComponent } from './about-me/skills/skills.component';
+import { VolunteeringComponent } from './about-me/volunteering/volunteering.component';
 import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'about-me', component: AboutMeComponent },
+  { path: 'career', component: CareerComponent },
+  { path: 'education', component: EducationComponent },
+  { path: 'volunteering', component: VolunteeringComponent },
+  { path: 'skills', component: SkillsComponent },
 ];
 
 @NgModule({
