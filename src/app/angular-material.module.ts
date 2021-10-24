15fcbf357e6fbd9660a8a1706535a4eef02cf9be
Minh Pham <minhnpham98@googlemail.com>
2021-10-24 02:21:40 +0200

    Added drop-down to "about-me"

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
 
 const AngularMaterialComponents = [
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
     MatToolbarModule,
 ];
 
