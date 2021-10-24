f379fbf6bbc9c989b190700609b356a014699ff0
Minh Pham <minhnpham98@googlemail.com>
2021-10-25 00:08:25 +0200

    Added first steps for "projects"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,9 @@ import { CareerComponent } from './about-me/career/career.component';
 import { EducationComponent } from './about-me/education/education.component';
 import { SkillsComponent } from './about-me/skills/skills.component';
 import { VolunteeringComponent } from './about-me/volunteering/volunteering.component';
+import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
@@ -15,6 +17,9 @@ const routes: Routes = [
   { path: 'education', component: EducationComponent },
   { path: 'volunteering', component: VolunteeringComponent },
   { path: 'skills', component: SkillsComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
+
 ];
 
 @NgModule({
