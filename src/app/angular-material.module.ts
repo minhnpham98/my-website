644fc7c6e69ddb72e7d0b28b5b77a6d8967727da
Minh Pham <minhnpham98@googlemail.com>
2021-11-02 23:07:22 +0100

    Changed stepper

--- src/app/angular-material.module.ts
+++ src/app/angular-material.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
@@ -10,6 +11,7 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 
 const AngularMaterialComponents = [
     MatButtonModule,
+    MatCardModule,
     MatGridListModule,
     MatIconModule,
     MatMenuModule,
